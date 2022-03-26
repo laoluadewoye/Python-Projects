5b940d7c6d41c88bc5e89caf62cec33d936adc51
Laolu <laoluadewoye@gmail.com>
2022-03-26 18:53:07 -0400

    The IDE inspected my formatting. These are the changes it made.
    This is a test (2) for commits to my Python storage.

--- Hamming Code Final.py
+++ Hamming Code Final.py
@@ -1,22 +1,23 @@
-'''
+"""
     The final self-exercise in making a hamming unicode
-    
+
     Today, I'm going to implement a RAID 0 concept called data striping on a large set of data
-    
+
     6 bits can store a number up to 63
-    
+
     So I'm going to generate a random amount of bits, then divide that into blocks of 63 bits per blocks
-    
-    Next, i'm going to have each block set it's own parity check.
-    
+
+    Next, I'm going to have each block set its own parity check.
+
     Lastly, the blocks will place their data back into the set.
-'''
+"""
 
 from functools import reduce
 import random
 import numpy as np
 
 import time
+
 
 def delay():
     print("4 seconds till refresh")
@@ -28,47 +29,48 @@ def delay():
     print("1 seconds till refresh")
     time.sleep(1)
 
-#Function to call for each array. 
+
+# Function to call for each array.
 def ParitySetup(bitSize):
     initError = reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bitSize) if bit])
     print("Index:", initError)
-    
-    #Turning that bit into binary form
+
+    # Turning that bit into binary form
     initErrorString = bin(initError)
-    
-    #Remove the '0b' label
+
+    # Remove the '0b' label
     initErrorStringAdjusted = initErrorString.replace('0b', '')
-    
+
     initErrorString = initErrorStringAdjusted
-    
+
     initErrorStringFormatted = ''.join(reversed(initErrorString))
-    
-    #Adding the zeros
-    while (len(initErrorStringFormatted) < 6):
+
+    # Adding the zeros
+    while len(initErrorStringFormatted) < 6:
         initErrorStringFormatted = initErrorStringFormatted + '0'
-    
-    #Step 3: Don't forget to reverse it back to proper state
+
+    # Step 3: Don't forget to reverse it back to proper state
     initErrorStringReversed = initErrorStringFormatted
     initErrorString = ''.join(reversed(initErrorStringFormatted))
-    
+
     print("Binary form:", initErrorString)
-    
+
     return initErrorStringReversed
 
-def PreparingArray(IESR, bitSize): #Init Error String Reversed
-    for i in range(len(IESR)):
-        if (IESR[i] == "1"):
-            if (bitSize[2**i] == 1):
-                bitSize[2**i] = 0
-            else:
-                bitSize[2**i] = 1
-            print("Parity Bit", 2**i, "was flipped.")
-            
-    return bitSize
-    
-    
 
-#First, create 8 default arrays
+def PreparingArray(IESR, bitSize):  # Init Error String Reversed
+    for i in range(len(IESR)):
+        if IESR[i] == "1":
+            if bitSize[2 ** i] == 1:
+                bitSize[2 ** i] = 0
+            else:
+                bitSize[2 ** i] = 1
+            print("Parity Bit", 2 ** i, "was flipped.")
+
+    return bitSize
+
+
+# First, create 8 default arrays
 blockLength = 63
 
 redundant1 = [0] * blockLength
@@ -80,14 +82,14 @@ redundant6 = [0] * blockLength
 redundant7 = [0] * blockLength
 redundant8 = [0] * blockLength
 
-#second, list them in a array of their own
+# second, list them in an array of their own
 
 redundantBlocks = [redundant1, redundant2, redundant3, redundant4, redundant5, redundant6, redundant7, redundant8]
 
-#third, generating the data
+# third, generating the data
 dataBank = np.random.randint(0, 2, random.randrange(0, 500))
 
-#np.random.randint(0, 2, random.randrange(0, 500))
+# np.random.randint(0, 2, random.randrange(0, 500))
 
 print(dataBank, end="\n\n")
 
@@ -95,7 +97,7 @@ dataBankLength = len(dataBank)
 
 print("Number of bits:", dataBankLength)
 
-#Configuring number of Arrays
+# Configuring number of Arrays
 
 numberOfArrays = dataBankLength // blockLength
 
@@ -112,22 +114,21 @@ print("Spillover bits:", spillOverLength)
 print("\nPreparing to strip data. Please wait.")
 delay()
 
-#Copying data into arrays
+# Copying data into arrays
 print()
 blockTick = 0
 index = 0
-for i in range (dataBankLength):
+for i in range(dataBankLength):
     redundantBlocks[blockTick][index] = dataBank[i]
     print("The value in index", index, "of array index", blockTick, "is", dataBank[i])
     time.sleep(0.05)
     blockTick += 1
-    if (blockTick == 8):
+    if blockTick == 8:
         blockTick = 0
         index += 1
 
 print("\nPreparing to display samples. Please wait.")
 delay()
-
 
 print()
 print("Sample of first array:  ", redundant1[0:6])
@@ -153,8 +154,7 @@ delay()
 print()
 print()
 
-
-#Preparing the blocks of data
+# Preparing the blocks of data
 print("Processing block 1...")
 parityValues = ParitySetup(redundant1)
 print("Sorting form:", parityValues)
@@ -221,17 +221,18 @@ print()
 print("\nPreparing to return data to original storage. Please wait.")
 delay()
 
-#Putting block data back into original storage
+# Putting block data back into original storage
 print()
 blockTick = 0
 index = 0
 
-for i in range (dataBankLength):
+for i in range(dataBankLength):
     dataBank[i] = redundantBlocks[blockTick][index]
-    print("The bit", redundantBlocks[blockTick][index], "in index", index, "of block", blockTick, "is inserted into index", i, "of the storage.")
+    print("The bit", redundantBlocks[blockTick][index], "in index", index, "of block", blockTick,
+          "is inserted into index", i, "of the storage.")
     time.sleep(0.05)
     blockTick += 1
-    if (blockTick == 8):
+    if blockTick == 8:
         index += 1
         blockTick = 0
 
