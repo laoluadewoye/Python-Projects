65fa504cbe0d1dde3fec0ba030dcc8dad71fe1ae
Laolu <laoluadewoye@gmail.com>
2022-03-26 18:42:20 -0400

    Minor Fixes
    This is a test for commits to my Python storage.

--- Affection level.py
+++ Affection level.py
@@ -1,14 +1,14 @@
-from random import *
+import random
 
 affection = 0
 afflevel = ("classmates", "friends", "lover", "wife", "yandere")
 level = 0
 
-while (affection < 110):
-    value = randint(1, 16)
+while affection < 110:
+    value = random.randint(1, 16)
     affection += value
     level = affection // 25
-    print("Affication is increased by", value, "Affection is now", affection)
+    print("Affection is increased by", value, "Affection is now", affection)
     print("Your affection level is now", afflevel[level])
 
 print()
